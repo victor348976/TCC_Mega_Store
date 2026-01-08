2a1340861444ed0c339ef5ed164fe6f3a2710777
victor348976 <victorluiz2504@gmail.com>
2026-01-08 16:00:11 -0300

    Cadastro_funcionado

---
+++ Cad_USER_funcional/logon.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Senha</title>
+</head>
+<body>
+    <center>
+        <form method="post" action="Logon.php">
+            <table border="0">
+                <tr>
+                    <td align="center" colspan="2">
+                      <h2><strong>Cadastro Usuário</strong></h2>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="right" width="25%">Usuário:
+                    </td>
+                    <td align="left" width="57%">
+                        <input type="text" value="" name="user"
+                        placeholder="Digite o Usuário">
+                    </td>
+                </tr>
+                <tr>
+                 <td align="right" width="25%">Senha: </td>
+                 <td align="left" width="57%">
+                  <div name="logsenha">
+                  <input type="password" name="logsenha" placeholder="Digite a Senha">
+                  <img src="olhorisc.png" name="logsenha">
+                  </div>
+                 </td>
+                </tr>
+                <tr>
+                 <td align="center" colspan="2">
+                  <br>
+                  <input type="submit" value="Entrar" name="Entrar">
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                  <input type="button" onclick="location.replace('logon.php');" value="Limpar"><br>
+                 </td>
+                </tr>
+            </table>
+        </form>
+        <script src="logsenha.js"></script>
+        <?php
+             if(isset($_POST["Entrar"])){
+                $user=$_POST["user"];
+                $senha=$_POST["logsenha"];
+                $erro='';
+                if($user==''){
+                    $erro="Digite o Usuário<br>";
+                }
+                if($senha==''){
+                    $erro.="Digite a Senha<br>";
+                } 
+                if($erro==''){
+                    session_start();
+                    if(isset($_SESSION['user'])and isset($_SESSION['senha'])){
+                        $cad_user=$_SESSION['user'];
+                        $cad_senha=$_SESSION['senha'];
+                    }else{
+                        echo"<font color='red' size='4'>Sem cadastros salvos</font>";
+                        exit;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+                    }
+                    if($user==$cad_user && $senha==$cad_senha){
+                        echo"<font color='green' size='4'>Bem Vindo</font>";
+                        echo"<form action='logon.php' method='post'>
+                                <input type='submit' value='Destruir' name='destruir'>
+                            </form>";
+                    }else{
+                        echo"<font color='red' size='4'>Senha e Usuário Incorretos</font>";
+                    }
+                }else{
+                    echo"<font color='red' size='4'>$erro</font>";
+                }
+             }
+             if(isset($_POST['destruir'])){
+                session_start();
+                session_destroy();
+                echo"<font color='red' size='4'>Sessão destruida com sucesso</font>";
+             }
+             if(!isset($_POST['Entrar'])){
+                echo"<form method='post' action='Cad_User.php'>
+                        <table border='0'>
+                            <tr>
+                                <td align='center'>
+                                    <font color='blue' size='2'>Cadastro Usuário</font>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td align='center'>
+                                    <input  type='submit' value='Cadastrar'> 
+                                </td>
+                            </tr>
+                        </table>
+                    </form>";
+             }
+        ?>
+    </center>
+</body>
+</html>
