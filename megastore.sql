82716270c9c75c303ccb30672fa3313d5e69d63f
victor348976 <victorluiz2504@gmail.com>
2026-03-26 19:55:39 -0300

    GRANDE UPD
    O interligado ainda não está completo, entretanto, servirá para que possamos "testar" a transição entre as telas

---
+++ megastore.sql
@@ -0,0 +1,471 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 19/03/2026 às 17:55
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `megastore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_categoria`
+--
+
+CREATE TABLE `tb_categoria` (
+  `id_categoria` int(11) NOT NULL,
+  `nome_categoria` varchar(255) NOT NULL,
+  `descricao_categoria` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_categoria`
+--
+
+INSERT INTO `tb_categoria` (`id_categoria`, `nome_categoria`, `descricao_categoria`) VALUES
+(1, 'Calça', 'É uma calça'),
+(2, 'Camiseta', 'É uma camiseta, n camisa, por favor, não confunda');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_endereco`
+--
+
+CREATE TABLE `tb_endereco` (
+  `id_endereco` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `rua` varchar(255) NOT NULL,
+  `numero` int(9) NOT NULL,
+  `bairro` int(255) NOT NULL,
+  `cidade` varchar(255) NOT NULL,
+  `estado` varchar(2) NOT NULL,
+  `cep` int(8) NOT NULL,
+  `complemento` varchar(300) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_forma_pagamento`
+--
+
+CREATE TABLE `tb_forma_pagamento` (
+  `id_forma_pagamento` int(11) NOT NULL,
+  `nome_forma_pagamento` varchar(150) NOT NULL,
+  `descricao_forma_pagamento` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_genero`
+--
+
+CREATE TABLE `tb_genero` (
+  `id_genero` int(11) NOT NULL,
+  `nome_genero` varchar(255) NOT NULL,
+  `descricao_genero` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_genero`
+--
+
+INSERT INTO `tb_genero` (`id_genero`, `nome_genero`, `descricao_genero`) VALUES
+(1, 'Masculino', 'Roupas para MACHOS DE VERDADE'),
+(2, 'Feminino', 'ROUPAS PARA AS MAIS DIVAS'),
+(3, 'Infantil', 'ROUPAS PARA AS PESTES MAIS LINDAS DO MUNDO'),
+(4, 'Unissex', 'VIVA AO GENERO FLUIDO, VIVA A COMUNIDADE LGBTQIAP+');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_imagem_produto`
+--
+
+CREATE TABLE `tb_imagem_produto` (
+  `id_imagem` int(11) NOT NULL,
+  `id_variacao` int(11) NOT NULL,
+  `caminho_imagem` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_imagem_produto`
+--
+
+INSERT INTO `tb_imagem_produto` (`id_imagem`, `id_variacao`, `caminho_imagem`) VALUES
+(1, 1, 'uploads/697037447dcb6.png'),
+(2, 2, 'uploads/697037448e0f3.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_pagamento`
+--
+
+CREATE TABLE `tb_pagamento` (
+  `id_pagamento` int(11) NOT NULL,
+  `id_venda` int(11) NOT NULL,
+  `id_forma_pagamento` int(11) NOT NULL,
+  `status_pagamento` tinyint(1) NOT NULL,
+  `data_pagamento` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_produto`
+--
+
+CREATE TABLE `tb_produto` (
+  `id_produto` int(11) NOT NULL,
+  `nome_produto` varchar(255) NOT NULL,
+  `descricao_produto` varchar(500) NOT NULL,
+  `preco` double(10,2) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `id_genero` int(11) NOT NULL,
+  `ativo` tinyint(1) NOT NULL,
+  `data_cadastro` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_produto`
+--
+
+INSERT INTO `tb_produto` (`id_produto`, `nome_produto`, `descricao_produto`, `preco`, `id_categoria`, `id_genero`, `ativo`, `data_cadastro`) VALUES
+(1, 'Joao', 'Joao sendo o Joao', 0.00, 2, 1, 1, '2026-01-21');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_produto_salvo`
+--
+
+CREATE TABLE `tb_produto_salvo` (
+  `id_produto_salvo` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_produto_salvo`
+--
+
+INSERT INTO `tb_produto_salvo` (`id_produto_salvo`, `id_produto`, `id_usuario`) VALUES
+(1, 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_produto_venda`
+--
+
+CREATE TABLE `tb_produto_venda` (
+  `id_produto_venda` int(11) NOT NULL,
+  `id_venda` int(11) NOT NULL,
+  `id_variacao` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_usuario`
+--
+
+CREATE TABLE `tb_usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nome_usuario` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `cpf` int(11) DEFAULT NULL,
+  `numero` int(11) DEFAULT NULL,
+  `tipo` tinyint(1) NOT NULL,
+  `data_cadastro` date NOT NULL,
+  `modo_tela` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_usuario`
+--
+
+INSERT INTO `tb_usuario` (`id_usuario`, `nome_usuario`, `email`, `senha`, `cpf`, `numero`, `tipo`, `data_cadastro`, `modo_tela`) VALUES
+(1, 'Gustavo', 'gutolindo@gmail.com', '$2y$10$MgSYDeXd7c6.VTj1aNZ/C.SlucbfWUqfJI80gMS4AuZm.jt1GnvBq', NULL, 40028922, 0, '2026-01-21', 0),
+(2, 'V', 'v@gmail.com', '$2y$10$oCU1Qbqnvuu6/n9PPXBBCeO0fewqGt.J9n7UpQGFeBubJ0gxNmkwu', NULL, 2147483647, 0, '2026-02-21', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_variacao_produto`
+--
+
+CREATE TABLE `tb_variacao_produto` (
+  `id_variacao` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL,
+  `tamanho` varchar(5) NOT NULL,
+  `cor` varchar(55) NOT NULL,
+  `estoque` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_variacao_produto`
+--
+
+INSERT INTO `tb_variacao_produto` (`id_variacao`, `id_produto`, `tamanho`, `cor`, `estoque`) VALUES
+(1, 1, 'GG', '#000000', 1),
+(2, 1, 'M', '#404040', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_venda`
+--
+
+CREATE TABLE `tb_venda` (
+  `id_venda` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `data_pedido` date NOT NULL,
+  `status_venda` varchar(55) NOT NULL,
+  `valor_total` double(12,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `tb_categoria`
+--
+ALTER TABLE `tb_categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Índices de tabela `tb_endereco`
+--
+ALTER TABLE `tb_endereco`
+  ADD PRIMARY KEY (`id_endereco`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Índices de tabela `tb_forma_pagamento`
+--
+ALTER TABLE `tb_forma_pagamento`
+  ADD PRIMARY KEY (`id_forma_pagamento`);
+
+--
+-- Índices de tabela `tb_genero`
+--
+ALTER TABLE `tb_genero`
+  ADD PRIMARY KEY (`id_genero`);
+
+--
+-- Índices de tabela `tb_imagem_produto`
+--
+ALTER TABLE `tb_imagem_produto`
+  ADD PRIMARY KEY (`id_imagem`),
+  ADD KEY `id_produto` (`id_variacao`);
+
+--
+-- Índices de tabela `tb_pagamento`
+--
+ALTER TABLE `tb_pagamento`
+  ADD PRIMARY KEY (`id_pagamento`),
+  ADD KEY `id_venda` (`id_venda`),
+  ADD KEY `forma_pagamento` (`id_forma_pagamento`);
+
+--
+-- Índices de tabela `tb_produto`
+--
+ALTER TABLE `tb_produto`
+  ADD PRIMARY KEY (`id_produto`),
+  ADD KEY `id_genero` (`id_genero`),
+  ADD KEY `id_categoria` (`id_categoria`);
+
+--
+-- Índices de tabela `tb_produto_salvo`
+--
+ALTER TABLE `tb_produto_salvo`
+  ADD PRIMARY KEY (`id_produto_salvo`),
+  ADD KEY `id_produto` (`id_produto`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Índices de tabela `tb_produto_venda`
+--
+ALTER TABLE `tb_produto_venda`
+  ADD PRIMARY KEY (`id_produto_venda`),
+  ADD KEY `id_venda` (`id_venda`),
+  ADD KEY `id_variacao` (`id_variacao`);
+
+--
+-- Índices de tabela `tb_usuario`
+--
+ALTER TABLE `tb_usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Índices de tabela `tb_variacao_produto`
+--
+ALTER TABLE `tb_variacao_produto`
+  ADD PRIMARY KEY (`id_variacao`),
+  ADD KEY `id_produto` (`id_produto`);
+
+--
+-- Índices de tabela `tb_venda`
+--
+ALTER TABLE `tb_venda`
+  ADD PRIMARY KEY (`id_venda`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_categoria`
+--
+ALTER TABLE `tb_categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `tb_endereco`
+--
+ALTER TABLE `tb_endereco`
+  MODIFY `id_endereco` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tb_forma_pagamento`
+--
+ALTER TABLE `tb_forma_pagamento`
+  MODIFY `id_forma_pagamento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tb_genero`
+--
+ALTER TABLE `tb_genero`
+  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `tb_imagem_produto`
+--
+ALTER TABLE `tb_imagem_produto`
+  MODIFY `id_imagem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `tb_pagamento`
+--
+ALTER TABLE `tb_pagamento`
+  MODIFY `id_pagamento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tb_produto`
+--
+ALTER TABLE `tb_produto`
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `tb_produto_salvo`
+--
+ALTER TABLE `tb_produto_salvo`
+  MODIFY `id_produto_salvo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `tb_produto_venda`
+--
+ALTER TABLE `tb_produto_venda`
+  MODIFY `id_produto_venda` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tb_usuario`
+--
+ALTER TABLE `tb_usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `tb_variacao_produto`
+--
+ALTER TABLE `tb_variacao_produto`
+  MODIFY `id_variacao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `tb_venda`
+--
+ALTER TABLE `tb_venda`
+  MODIFY `id_venda` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `tb_endereco`
+--
+ALTER TABLE `tb_endereco`
+  ADD CONSTRAINT `tb_endereco_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuario` (`id_usuario`);
+
+--
+-- Restrições para tabelas `tb_imagem_produto`
+--
+ALTER TABLE `tb_imagem_produto`
+  ADD CONSTRAINT `tb_imagem_produto_ibfk_1` FOREIGN KEY (`id_variacao`) REFERENCES `tb_variacao_produto` (`id_variacao`);
+
+--
+-- Restrições para tabelas `tb_pagamento`
+--
+ALTER TABLE `tb_pagamento`
+  ADD CONSTRAINT `tb_pagamento_ibfk_1` FOREIGN KEY (`id_venda`) REFERENCES `tb_venda` (`id_venda`),
+  ADD CONSTRAINT `tb_pagamento_ibfk_2` FOREIGN KEY (`id_forma_pagamento`) REFERENCES `tb_forma_pagamento` (`id_forma_pagamento`);
+
+--
+-- Restrições para tabelas `tb_produto`
+--
+ALTER TABLE `tb_produto`
+  ADD CONSTRAINT `tb_produto_ibfk_1` FOREIGN KEY (`id_genero`) REFERENCES `tb_genero` (`id_genero`),
+  ADD CONSTRAINT `tb_produto_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `tb_categoria` (`id_categoria`);
+
+--
+-- Restrições para tabelas `tb_produto_salvo`
+--
+ALTER TABLE `tb_produto_salvo`
+  ADD CONSTRAINT `tb_produto_salvo_ibfk_1` FOREIGN KEY (`id_produto`) REFERENCES `tb_produto` (`id_produto`),
+  ADD CONSTRAINT `tb_produto_salvo_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuario` (`id_usuario`);
+
+--
+-- Restrições para tabelas `tb_produto_venda`
+--
+ALTER TABLE `tb_produto_venda`
+  ADD CONSTRAINT `tb_produto_venda_ibfk_1` FOREIGN KEY (`id_venda`) REFERENCES `tb_venda` (`id_venda`),
+  ADD CONSTRAINT `tb_produto_venda_ibfk_2` FOREIGN KEY (`id_variacao`) REFERENCES `tb_variacao_produto` (`id_variacao`);
+
+--
+-- Restrições para tabelas `tb_variacao_produto`
+--
+ALTER TABLE `tb_variacao_produto`
+  ADD CONSTRAINT `tb_variacao_produto_ibfk_1` FOREIGN KEY (`id_produto`) REFERENCES `tb_produto` (`id_produto`);
+
+--
+-- Restrições para tabelas `tb_venda`
+--
+ALTER TABLE `tb_venda`
+  ADD CONSTRAINT `tb_venda_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuario` (`id_usuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
