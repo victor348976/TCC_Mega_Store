883af6f176b9d613eb0f7f296ec89d49228205cb
pedrohqp3DS <pedrohqporetto@gmail.com>
2026-04-02 15:09:36 -0300

    Add files via upload

---
+++ PORETTO/index.php
@@ -0,0 +1,17 @@
+<?php
+$texto = "IMAGEM<br>DENTRO<br>DO TEXTO";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<h1 class="texto"><?php echo $texto; ?></h1>
+
+</body>
+</html>
